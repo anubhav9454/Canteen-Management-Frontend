5f884a87752864fdd4ef809d1f6c46991001f9fa
Manupal Choudhary <hi@manupal.dev>
2022-11-28 05:56:50 +0000

    Create 3 files

---
+++ pages/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> 
+    <Title value='Breakfast' link='/'/>
+    <Title value='Lunch' link='/' />
+  </div>
+</template>
+
+<script>
+import Title from '../components/menu_tile'
+
+export default {
+  components:{
+    Title
+  }
+}
+
+</script>
